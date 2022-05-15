9eff1afa1b58190eef464c066587c730c4da7598
Andre <andrepalhares1989@gmail.com>
2022-05-15 18:25:40 +0100

    changing ReactDOM render call to use createRoot because of depecration reason

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Application from './app/App';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import Application from "./app/App";
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+const root = createRoot(document.getElementById("root") as Element);
+root.render(<Application />);
