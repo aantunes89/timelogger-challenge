f59e006798cd6aa616a4926058ed48cdf4fdf058
Andre <andrepalhares1989@gmail.com>
2022-05-17 00:35:56 +0100

    updating table styles

--- client/src/app/components/Table/styles.ts
+++ client/src/app/components/Table/styles.ts
@@ -1,15 +1,24 @@
+import { Grid, GridProps } from "@mui/material";
 import styled from "styled-components";
 
-export const ContentContainer = styled.div`
-  color: var(--text-title);
+export const ContentContainer = styled(Grid)<GridProps>(() => ({
+  width: "100%",
+  color: "var(--text-title)",
+  label: {
+    color: "var(--text-body)",
+    marginRight: ".5rem",
+  },
 
-  label {
-    color: var(--text-body);
-    margin-right: 0.8rem;
-  }
+  p: {
+    width: "100%",
+    display: "flex",
+  },
 
-  p {
-    display: inline-block !important;
-    margin-right: 2rem !important;
-  }
-`;
+  ".truncate": {
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    width: "100%",
+    display: "inline-block",
+  },
+}));
