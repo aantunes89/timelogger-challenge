8650863a2e74065ce3d0a7d2e1f9681c8b1c3059
Andre <andrepalhares1989@gmail.com>
2022-05-16 16:05:55 +0100

    setting up global state to check the type of overview we want to see

--- client/src/app/components/Table/Table.tsx
+++ client/src/app/components/Table/Table.tsx
@@ -4,36 +4,52 @@ import { useScreenState } from "../../hooks/useScreenState";
 import { Project } from "../../models/Project";
 
 export default function Table() {
-  const { projects, setProjectId } = useProjects();
-  const { setModalOpen } = useScreenState();
+  const { projects, projectId, setProjectId } = useProjects();
+  const {
+    setModalOpen,
+    isFreelancerOverviewVisible,
+    setFreelancerOverviewVisible,
+  } = useScreenState();
 
   function addEntry(projectId: number) {
     setProjectId(projectId);
     setModalOpen(true);
   }
 
+  function toggleFreelancerOverview() {
+    setFreelancerOverviewVisible(!isFreelancerOverviewVisible);
+  }
+
   function renderProjects(projects: Project[]) {
     return projects.map(({ id, name }) => (
-      <tr key={id} onClick={() => addEntry(id)}>
+      <tr key={id}>
         <td className="border px-4 py-2 w-12">{id}</td>
         <td className="border px-4 py-2">{name}</td>
-        <td className="border px-4 py-2">abc</td>
-        <td className="border px-4 py-2">xyz</td>
+        <td className="border px-4 py-2">
+          <button onClick={() => toggleFreelancerOverview()}>
+            Show Overview
+          </button>
+        </td>
+        <td className="border px-4 py-2">
+          <button onClick={() => addEntry(id)}>Add Entry</button>
+        </td>
       </tr>
     ));
   }
 
   return (
-    <table className="table-fixed w-full">
-      <thead className="bg-gray-200">
-        <tr>
-          <th className="border px-4 py-2 w-12">#</th>
-          <th className="border px-4 py-2">Project Name</th>
-          <th className="border px-4 py-2">abc</th>
-          <th className="border px-4 py-2">xyz</th>
-        </tr>
-      </thead>
-      <tbody>{projects && renderProjects(projects)}</tbody>
-    </table>
+    <>
+      <table className="table-fixed w-full">
+        <thead className="bg-gray-200">
+          <tr>
+            <th className="border px-4 py-2 w-12">#</th>
+            <th className="border px-4 py-2">Project Name</th>
+            <th className="border px-4 py-2">abc</th>
+            <th className="border px-4 py-2">xyz</th>
+          </tr>
+        </thead>
+        <tbody>{projects && renderProjects(projects)}</tbody>
+      </table>
+    </>
   );
 }
--- client/src/app/hooks/useScreenState.tsx
+++ client/src/app/hooks/useScreenState.tsx
@@ -7,6 +7,8 @@ interface ScreenStateProviderProps {
 interface ScreenStateContextData {
   isModalOpen: boolean;
   setModalOpen: (value: boolean) => void;
+  isFreelancerOverviewVisible: boolean;
+  setFreelancerOverviewVisible: (value: boolean) => void;
 }
 
 const ScreenStateContext = createContext<ScreenStateContextData>(
@@ -16,8 +18,18 @@ const ScreenStateContext = createContext<ScreenStateContextData>(
 export function ScreenStateProvider({ children }: ScreenStateProviderProps) {
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
 
+  const [isFreelancerOverviewVisible, setFreelancerOverviewVisible] =
+    useState<boolean>(false);
+
   return (
-    <ScreenStateContext.Provider value={{ isModalOpen, setModalOpen }}>
+    <ScreenStateContext.Provider
+      value={{
+        isModalOpen,
+        setModalOpen,
+        isFreelancerOverviewVisible,
+        setFreelancerOverviewVisible,
+      }}
+    >
       {children}
     </ScreenStateContext.Provider>
   );
