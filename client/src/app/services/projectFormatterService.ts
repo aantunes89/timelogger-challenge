f9fd774e7cf5c5c8b98b11a742a69937d054bf4b
Andre <andrepalhares1989@gmail.com>
2022-05-17 00:36:32 +0100

    adding totalTimeSpent to project payload arr

--- client/src/app/services/projectFormatterService.ts
+++ client/src/app/services/projectFormatterService.ts
@@ -14,6 +14,11 @@ export function setupProjectPayload(projects: Project[], entries: Entry[]) {
     const totalPricesArr = projectEntries.map(({ totalPrice }) => totalPrice);
     project.totalPrice = totalPricesArr.reduce((acc, curr) => acc + curr, 0);
 
+    // set total time
+
+    const totalTimeArr = projectEntries.map(({ timeSpent }) => timeSpent);
+    project.totalTimeSpent = totalTimeArr.reduce((acc, curr) => acc + curr, 0);
+
     return project;
   });
 }
