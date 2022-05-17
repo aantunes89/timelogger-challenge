a82fcc6e8ef50ae0b3138cae538fb0eda0297889
Andre <andrepalhares1989@gmail.com>
2022-05-17 17:12:26 +0100

    creating styles file for Projects view

---
+++ client/src/app/views/styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 3rem;
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 1.5rem 0;
+`;
