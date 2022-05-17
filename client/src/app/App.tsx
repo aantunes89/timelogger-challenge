2712be170b888e1518dc39be54f204270a94cdc7
Andre <andrepalhares1989@gmail.com>
2022-05-17 17:10:09 +0100

    removing old dependencies, updating dependencies and removing tailwindcss

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,27 +1,28 @@
 import * as React from "react";
 import Projects from "./views/Projects";
-import "./tailwind.generated.css";
 import { GlobalStyle } from "./styles/global";
-import { ProjectsProvider } from "./hooks/useProjects";
 import { ScreenStateProvider } from "./hooks/useScreenState";
+
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 
 export default function App() {
   return (
     <ScreenStateProvider>
       <GlobalStyle />
-      <header className="bg-gray-900 text-white flex items-center h-12 w-full">
-        <div className="container mx-auto">
-          <a className="navbar-brand" href="/">
-            Timelogger
-          </a>
-        </div>
-      </header>
 
-      <main>
-        <div className="container mx-auto">
-          <Projects />
-        </div>
-      </main>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Timelogger
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Box sx={{ flexGrow: 1 }} padding={4}>
+        <Projects />
+      </Box>
     </ScreenStateProvider>
   );
 }
--- client/src/app/views/Projects.tsx
+++ client/src/app/views/Projects.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Box } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
-import { NewEntryModal } from "../components/NewEntryModal/NewEntryModal";
 import Table from "../components/Table/Table";
+import { NewEntryModal } from "../components/NewEntryModal/NewEntryModal";
 import { RoundButton } from "../components/RoundButton/styles";
+import { Container } from "./styles";
+
 import { useScreenState } from "../hooks/useScreenState";
 
 export default function Projects() {
@@ -16,28 +18,25 @@ export default function Projects() {
 
   return (
     <>
-      <div className="flex items-center my-6">
-        <div className="w-1/2">
+      <Container>
+        <Box>
           <Button size="large" variant="contained">
             Add Project
           </Button>
-        </div>
+        </Box>
+        <Box>
+          <TextField
+            size="small"
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+          />
 
-        <div className="w-1/2 flex justify-end align-center">
-          <form>
-            <TextField
-              size="small"
-              id="outlined-basic"
-              label="Search"
-              variant="outlined"
-            />
-
-            <RoundButton variant="contained">
-              <SearchIcon />
-            </RoundButton>
-          </form>
-        </div>
-      </div>
+          <RoundButton variant="contained">
+            <SearchIcon />
+          </RoundButton>
+        </Box>
+      </Container>
 
       <NewEntryModal
         isOpen={isModalOpen}
