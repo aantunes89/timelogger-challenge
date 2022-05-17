85af8b940e449a10e16bb3d431ea59275f9eadd1
Andre <andrepalhares1989@gmail.com>
2022-05-17 23:07:19 +0100

    abstracting the payload enrichment from useProjects

--- client/src/app/api/projects.ts
+++ client/src/app/api/projects.ts
@@ -1,7 +1,25 @@
 import axios from "axios";
+import { Project } from "../models/Project";
+import { setupProjectPayload } from "../services/projectFormatterService";
 
 const BASE_URL = "http://localhost:3001/api";
 
 export const axiosApiService = axios.create({
   baseURL: BASE_URL,
 });
+
+export async function getAllProjects(): Promise<{ projects: Project[] }> {
+  return axiosApiService
+    .get<Project[]>("/projects")
+    .then(({ data }) => setupProjectPayload(data))
+    .then((projects) => ({
+      projects,
+    }));
+}
+
+export async function addProjec(project: Partial<Project>) {
+  return axiosApiService
+    .post("/projects")
+    .then(console.log)
+    .then((response) => response);
+}
