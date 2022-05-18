fc32025769fd208872b2672bf00706f820df5ba0
Andre <andrepalhares1989@gmail.com>
2022-05-18 22:27:28 +0100

    setting up modal setappelement on root

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import Application from "./app/App";
+import Modal from "react-modal";
 
 const root = createRoot(document.getElementById("root") as Element);
 root.render(<Application />);
+
+Modal.setAppElement("#root");
