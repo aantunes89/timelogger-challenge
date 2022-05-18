b4b2adb6009c6d57ec7ce70931a26c890b756a3a
Andre <andrepalhares1989@gmail.com>
2022-05-18 17:40:07 +0100

    moving tests to tests folder

--- client/src/app/tests/App.spec.tsx
+++ client/src/app/tests/App.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
-import App from "./App";
-import Projects from "./views/Projects";
+import App from "../App";
+import Projects from "../views/Projects";
 
 describe("App Component", () => {
   it("should render all elements of AppBar", () => {
