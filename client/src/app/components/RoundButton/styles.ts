c0ec771bfbed2fc421e2523b4578df197fe3ff64
Andre <andrepalhares1989@gmail.com>
2022-05-19 22:44:04 +0100

    removing unused round button

--- client/src/app/components/RoundButton/styles.ts
+++
@@ -1,7 +0,0 @@
-import { Button, ButtonProps } from "@mui/material";
-import styled from "styled-components";
-
-export const RoundButton = styled(Button)<ButtonProps>(() => ({
-  borderRadius: "100px !important",
-  marginLeft: ".5rem !important",
-}));
